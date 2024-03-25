6e3113f31cddcb1d266938e29b1a98d92d67eae7
Jinhai912 <75198453+Jinhai912@users.noreply.github.com>
2024-03-25 15:47:00 +0800

    Add files via upload

---
+++ Assignment4/ConsoleApp1/Program.cs
@@ -0,0 +1,88 @@
+﻿// 链表节点
+public class Node<T>
+{
+    public Node<T> Next { get; set; }
+    public T Data { get; set; }
+
+    public Node(T t)
+    {
+        Next = null;
+        Data = t;
+    }
+}
+//泛型链表类
+public class GenericList<T>
+{
+    private Node<T> head;
+    private Node<T> tail;
+
+    public GenericList()
+    {
+        tail = head = null;
+    }
+
+    public Node<T> Head
+    {
+        get => head;
+    }
+
+    public void Add(T t)
+    {
+        Node<T> n = new Node<T>(t);
+        if (tail == null)
+        {
+            head = tail = n;
+        }
+        else
+        {
+            tail.Next = n;
+            tail = n;
+        }
+    }
+    //PPT源码
+    
+    public void ForEach(Action<T> action)
+    {
+        Node<T> temp = head;
+        while (temp.Next != null)
+        {
+            action(temp.Data);
+            temp = temp.Next;
+        }
+        action(temp.Data);
+    }//泛型链表的遍历方法
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        GenericList<int> list = new GenericList<int>();
+        //随便赋一组值
+        for (int i = 0; i < 20; i += 2)
+        {
+            list.Add(i);
+        }
+
+        //打印元素
+        Console.WriteLine("打印所有元素：");
+        list.ForEach(i => Console.WriteLine(i));
+
+        //求最大值
+        Console.WriteLine("求所有元素的最大值:");
+        int max = 0;
+        list.ForEach(i => {if (max < i) max = i; });
+        Console.WriteLine(max);
+
+        //求最小值
+        Console.WriteLine("求所有元素的最小值:");
+        int min = 0;
+        list.ForEach(i => { if (min > i) min = i; });
+        Console.WriteLine(min);
+
+        //求和
+        Console.WriteLine("求所有元素之和:");
+        int sum = 0;
+        list.ForEach(i => sum += i);
+        Console.WriteLine(sum);
+    }
+}
---
+++ Assignment4/ConsoleApp2/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+class Clock
+{
+    // 定义滴答事件
+    public event EventHandler Tiktok;
+    // 定义响铃事件
+    public event EventHandler Alarm;
+
+    // 触发滴答事件
+    protected virtual void OnTink(EventArgs e)
+    {
+        Tiktok?.Invoke(this, e);
+    }
+
+    // 触发响铃事件
+    protected virtual void OnAlarm(EventArgs e)
+    {
+        Alarm?.Invoke(this, e);
+    }
+
+    // 启动闹钟
+    public void Start()
+    {
+        // 模拟时间流逝
+        for (int i = 0; i < 10; i++)
+        {
+            // 触发滴答事件
+            OnTink(EventArgs.Empty);
+
+            // 模拟响铃：五秒响一次铃
+            if (i == 5)
+            {
+                // 触发响铃事件
+                OnAlarm(EventArgs.Empty);
+            }
+
+            // 等待一秒钟
+            System.Threading.Thread.Sleep(1000);
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Clock clock = new Clock();
+
+        // 订阅滴答事件的处理方法
+        clock.Tiktok += (sender, e) =>
+        {
+            Console.WriteLine("滴答");
+        };
+
+        // 订阅响铃事件的处理方法
+        clock.Alarm += (sender, e) =>
+        {
+            Console.WriteLine("响铃");
+        };
+
+        // 启动闹钟
+        clock.Start();
+    }
+}
